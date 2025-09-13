8cfa764a4bc6124908c5b7140fb09e4e033404b2
ctrindadedev <caiomedtrindade@gmail.com>
2025-09-13 16:59:41 -0300

    fix: adição de getters e setters nos models

--- src/main/java/ufrn/imd/lp2/lab01/domain/Item.java
+++ src/main/java/ufrn/imd/lp2/lab01/domain/Item.java
@@ -3,12 +3,33 @@ package main.java.ufrn.imd.lp2.lab01.domain;
 import java.util.ArrayList;
 
 public class Item {
-    public String name;
-    public Float price;
+
+    private String name;
+    private Double price;
 
 
-    public Item(Float price, String name) {
-        this.price = price;
+    public Item( String name, Double price) {
         this.name = name;
+        if (price > 0) {
+            this.price = price;
+        }
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        if (price > 0) {
+            this.price = price;
+        }
     }
 }
--- src/main/java/ufrn/imd/lp2/lab01/domain/Pedido.java
+++ src/main/java/ufrn/imd/lp2/lab01/domain/Pedido.java
@@ -1,46 +1,56 @@
 package main.java.ufrn.imd.lp2.lab01.domain;
-
-
+import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Pedido {
 
+    private Integer idPedido;
+    private String nomeClientePedido;
+    private List<Item> itens;
+    private Double valorTotalPedido;
 
 
-    private Integer idPedido = 0;
-    public String nomeClientePedido;
-    public Integer quantidadeItemPedido;
-    public Item itemPedido;
-    public ArrayList<Item> itens;
-
-
-    public Pedido( String nomeClientePedido, Integer quantidadeItemPedido, Item itemPedido) {
-        this.idPedido += 1;
-        this.nomeClientePedido = nomeClientePedido;
-        this.quantidadeItemPedido = quantidadeItemPedido;
-        this.itemPedido = itemPedido;
-        this.itens = new ArrayList<>();
-    }
-
-    public void setNumero(int novoId) {
+    public Pedido() {
+        this.itens = new ArrayList<>(); // Garante que a lista nunca seja nula
     }
 
     public Integer getIdPedido() {
         return idPedido;
     }
 
+    //Metódo usado pelo PedidoRepository para setar o ID
+    public void setIdPedido(Integer idPedido) {
+        this.idPedido = idPedido;
+    }
+
+
+    public String getNomeClientePedido() {
+        return nomeClientePedido;
+    }
+
     public void setNomeCliente(String nomeCliente) {
         this.nomeClientePedido = nomeCliente;
     }
 
-    public void setItens(ArrayList<Item> itens) {
+    public List<Item> getItens() {
+        return itens;
+    }
+
+    public void setItens(List<Item> itens) {
         this.itens = itens;
     }
 
+    public Double getValorTotal() {
+        return this.valorTotalPedido;
+    }
+
     public void calcularTotal() {
-        float total = 0;
-        for (Item item : itens) {
-            total += item.price;
+        this.valorTotalPedido = 0.0;
+        if (this.itens != null) {
+            for (Item item : this.itens) {
+                this.valorTotalPedido += item.getPrice();
+            }
         }
     }
 }
